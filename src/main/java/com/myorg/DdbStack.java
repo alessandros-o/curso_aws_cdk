30482882c41aab9f098586d7e46307de535ec695
Alessandro da Silva Oliveira - AOl <alessandro.oliveira@iteris.com.br>
2023-12-07 07:54:40 -0300

    feat: alterando configurações DDB para ligar autoscaling

--- src/main/java/com/myorg/DdbStack.java
+++ src/main/java/com/myorg/DdbStack.java
@@ -1,12 +1,10 @@
 package com.myorg;
 
+import software.amazon.awscdk.Duration;
 import software.amazon.awscdk.RemovalPolicy;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
-import software.amazon.awscdk.services.dynamodb.Attribute;
-import software.amazon.awscdk.services.dynamodb.AttributeType;
-import software.amazon.awscdk.services.dynamodb.BillingMode;
-import software.amazon.awscdk.services.dynamodb.Table;
+import software.amazon.awscdk.services.dynamodb.*;
 import software.constructs.Construct;
 
 public class DdbStack extends Stack {
@@ -36,6 +34,26 @@ public class DdbStack extends Stack {
                 .timeToLiveAttribute("ttl")
                 .removalPolicy(RemovalPolicy.DESTROY)
                 .build();
+
+        productEventsddb.autoScaleReadCapacity(EnableScalingProps.builder()
+                .minCapacity(1)
+                .maxCapacity(4)
+                .build())
+                .scaleOnUtilization(UtilizationScalingProps.builder()
+                        .targetUtilizationPercent(50)//configuração para que se chegar em 50% do alvo...
+                        .scaleInCooldown(Duration.seconds(30))//dentro de 30seg já liga o mecanismo
+                        .scaleOutCooldown(Duration.seconds(30))
+                        .build());
+
+        productEventsddb.autoScaleWriteCapacity(EnableScalingProps.builder()
+                        .minCapacity(1)
+                        .maxCapacity(4)
+                        .build())
+                .scaleOnUtilization(UtilizationScalingProps.builder()
+                        .targetUtilizationPercent(50)//configuração para que se chegar em 50% do alvo...
+                        .scaleInCooldown(Duration.seconds(30))//dentro de 30seg já liga o mecanismo
+                        .scaleOutCooldown(Duration.seconds(30))
+                        .build());
     }
 
     public Table getProductEventsddb() {
